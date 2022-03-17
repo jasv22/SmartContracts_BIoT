5b94bca5186f1a71bf644c369cd4c0c1296c9c04
Jose Sanchez <54476439+josesanchez2019@users.noreply.github.com>
2022-03-17 13:29:57 -0400

    Update BIoT_NFT.sol

--- BIoT_NFT.sol
+++ BIoT_NFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 //https://remix.ethereum.org/
 pragma solidity >=0.8.0 <0.9.0;
-import "./erc721-master/src/contracts/tokens/nf-token-metadata.sol";
-import "./erc721-master/src/contracts/ownership/ownable.sol";
+import "https://github.com/josesanchez2019/SmartContracts_BIoT/blob/main/erc721-master/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/josesanchez2019/SmartContracts_BIoT/blob/main/erc721-master/src/contracts/ownership/ownable.sol";
 
 contract IoT is NFTokenMetadata, Ownable{
 
