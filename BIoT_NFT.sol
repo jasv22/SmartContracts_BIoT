fa5b31ff2c40a7ee96c59f2dc6121c111736fe7e
Jose Sanchez <54476439+josesanchez2019@users.noreply.github.com>
2022-03-17 13:23:04 -0400

    Add files via upload

---
+++ BIoT_NFT.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0
+//https://remix.ethereum.org/
+pragma solidity >=0.8.0 <0.9.0;
+import "./erc721-master/src/contracts/tokens/nf-token-metadata.sol";
+import "./erc721-master/src/contracts/ownership/ownable.sol";
+
+contract IoT is NFTokenMetadata, Ownable{
+
+    constructor() {
+        nftName = "IoT Blockchain";
+        nftSymbol = "BIoT";
+    }
+
+    function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
+        super._mint(_to, _tokenId);
+        super._setTokenUri(_tokenId, _uri);
+    }
+
+    struct msg_iot {
+        string message;
+        uint256 timestamp;
+    }
+
+   /* event Message (
+        address indexed sender,
+        string message,
+        uint256 timestamp,
+        uint256 tokenId
+    );*/
+
+    mapping(uint256 => msg_iot) public getTopicMessage;
+    msg_iot private message_struct;
+    
+    function addMessage(string memory _message, uint256 _tokenId) public {
+        require(msg.sender == super.ownerOf(_tokenId));
+        message_struct = msg_iot(_message, block.timestamp);
+        //emit Message(msg.sender, _message, block.timestamp, _tokenId);
+        getTopicMessage[_tokenId] = message_struct;
+    }
+    
+}
